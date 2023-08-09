8ffbcff24857fe3bb9349f0fecdc87e7051741d6
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2023-08-09 21:20:11 +0530

    Create 75. Sort Colors.cpp
    https://leetcode.com/problems/sort-colors/description/
    
    Use Of algo Dutch national Flag Algo 
    
    Maintain Low = 0 , mid = 0 , high = size -1 ;
    while(mid<=high)   perform loop , if mid element is 0  then swap low and mid , increase low and mid
    if mid element is 1 , then just increase the mid 
    if mid element is 2 , then swap mid and high , and decrease high

---
+++ Array/Medium/75. Sort Colors.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int x1 = 0 ;
+        int x2 = 0 ; 
+        int x3 = 0 ;
+        for(int i = 0 ; i<nums.size();i++){
+            if(nums[i]==0) x1++;
+            if(nums[i]==1) x2++;
+            if(nums[i]==2) x3++;
+        }
+        nums.clear();
+        int i = 0;
+
+            while(x1>0){
+                nums.push_back(0);
+                x1--;
+            }
+            while(x2>0){
+                nums.push_back(1);
+                x2--;
+            }
+            while(x3>0){
+                nums.push_back(2);
+                x3--;
+            }
+    }
+};
+class Solution{
+    public:
+    void sortColors(vector<int>& nums){
+        int low = 0 ; 
+        int mid = 0 ;
+        int high = nums.size()-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
