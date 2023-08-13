e8463b4b0831e14c0dbeae31a2e9311c5773825b
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2023-08-13 19:39:10 +0530

    Update 169. Majority Element.cpp

--- Array/Easy/169. Majority Element.cpp
+++ Array/Easy/169. Majority Element.cpp
@@ -1,9 +1,12 @@
 Approach 1: Sorting
 Intuition:
-The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted. Therefore, we can sort the array and return the element at index n/2.
+The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array),
+    it will always occupy the middle position when the array is sorted. 
+    Therefore, we can sort the array and return the element at index n/2.
 
 Explanation:
-The code begins by sorting the array nums in non-decreasing order using the sort function from the C++ Standard Library. This rearranges the elements such that identical elements are grouped together.
+The code begins by sorting the array nums in non-decreasing order using the sort function from the C++ Standard Library. 
+    This rearranges the elements such that identical elements are grouped together.
 Once the array is sorted, the majority element will always be present at index n/2, where n is the size of the array.
 This is because the majority element occurs more than n/2 times, and when the array is sorted, it will occupy the middle position.
 The code returns the element at index n/2 as the majority element.
@@ -21,7 +24,8 @@ public:
 ************************---
 Approach 2: Hash Map
 Intuition:
-The intuition behind using a hash map is to count the occurrences of each element in the array and then identify the element that occurs more than n/2 times. By storing the counts in a hash map, we can efficiently keep track of the occurrences of each element.
+The intuition behind using a hash map is to count the occurrences of each element in the array and then identify the element that occurs more than n/2 times. 
+    By storing the counts in a hash map, we can efficiently keep track of the occurrences of each element.
 
 Explanation:
 
@@ -30,13 +34,15 @@ It then iterates through the array nums using a for loop.
 For each element nums[i], it increments its count in the hash map m by using the line m[nums[i]]++;.
 If nums[i] is encountered for the first time, it will be added to the hash map with a count of 1.
 If nums[i] has been encountered before, its count in the hash map will be incremented by 1.
-After counting the occurrences of each element, the code updates n to be n/2, where n is the size of the array. This is done to check if an element occurs more than n/2 times, which is the criteria for being the majority element.
+After counting the occurrences of each element, the code updates n to be n/2, where n is the size of the array. 
+    This is done to check if an element occurs more than n/2 times, which is the criteria for being the majority element.
 The code then iterates through the key-value pairs in the hash map using a range-based for loop.
 For each key-value pair (x.first, x.second), it checks if the count x.second is greater than n.
 If the count is greater than n, it means that x.first occurs more than n/2 times, so it returns x.first as the majority element.
 If no majority element is found in the hash map, the code returns 0 as the default value.
 Note that this will only occur if the input array nums is empty or does not have a majority element.
-The time complexity of this approach is O(n) because it iterates through the array once to count the occurrences and then iterates through the hash map, which has a maximum size of the number of distinct elements in the array.
+The time complexity of this approach is O(n) because it iterates through the array once to count the occurrences and then iterates through the hash map,
+which has a maximum size of the number of distinct elements in the array.
 
 class Solution {
 public:
@@ -60,7 +66,8 @@ public:
 ************************-----------------
   Approach 3: Moore Voting Algorithm
 Intuition:
-The intuition behind the Moore's Voting Algorithm is based on the fact that if there is a majority element in an array, it will always remain in the lead, even after encountering other elements.
+The intuition behind the Moore's Voting Algorithm is based on the fact that if there is a majority element in an array, 
+    it will always remain in the lead, even after encountering other elements.
 
 Explanation:
 Algorithm:
@@ -75,20 +82,26 @@ Explanation:
 
 The algorithm starts by assuming the first element as the majority candidate and sets the count to 1.
 As it iterates through the array, it compares each element with the candidate:
-a. If the current element matches the candidate, it suggests that it reinforces the majority element because it appears again. Therefore, the count is incremented by 1.
-b. If the current element is different from the candidate, it suggests that there might be an equal number of occurrences of the majority element and other elements. Therefore, the count is decremented by 1.
+a. If the current element matches the candidate, it suggests that it reinforces the majority element because it appears again. 
+    Therefore, the count is incremented by 1.
+b. If the current element is different from the candidate, 
+it suggests that there might be an equal number of occurrences of the majority element and other elements. 
+    Therefore, the count is decremented by 1.
 Note that decrementing the count doesn't change the fact that the majority element occurs more than n/2 times.
-If the count becomes 0, it means that the current candidate is no longer a potential majority element. In this case, a new candidate is chosen from the remaining elements.
+If the count becomes 0, it means that the current candidate is no longer a potential majority element.
+In this case, a new candidate is chosen from the remaining elements.
 The algorithm continues this process until it has traversed the entire array.
 The final value of the candidate variable will hold the majority element.
 Explanation of Correctness:
-The algorithm works on the basis of the assumption that the majority element occurs more than n/2 times in the array. This assumption guarantees that even if the count is reset to 0 by other elements, the majority element will eventually regain the lead.
+The algorithm works on the basis of the assumption that the majority element occurs more than n/2 times in the array. 
+    This assumption guarantees that even if the count is reset to 0 by other elements, the majority element will eventually regain the lead.
 
 Let's consider two cases:
 
 If the majority element has more than n/2 occurrences:
 
-The algorithm will ensure that the count remains positive for the majority element throughout the traversal, guaranteeing that it will be selected as the final candidate.
+The algorithm will ensure that the count remains positive for the majority element throughout the traversal, 
+    guaranteeing that it will be selected as the final candidate.
 If the majority element has exactly n/2 occurrences:
 
 In this case, there will be an equal number of occurrences for the majority element and the remaining elements combined.
