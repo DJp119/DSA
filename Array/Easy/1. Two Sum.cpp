fbb78821ac850dd8e0a8c23dd8fc6c08708b2c6e
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2023-08-14 14:20:42 +0530

    Create 1. Two Sum.cpp

---
+++ Array/Easy/1. Two Sum.cpp
@@ -0,0 +1,30 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for(int i = 0 ; i<nums.size();i++){
+//             for(int j= i+1;j<nums.size();j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return {i,j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+
+class Solution{
+    public:
+    vector<int> twoSum(vector<int> &nums,int target){
+        unordered_map<int,int> mp;
+
+        for(int i = 0 ; i < nums.size();i++){
+            if(mp.find(target-nums[i]) == mp.end() )     mp[nums[i]] = i;
+            
+            else return{mp[target-nums[i]],i};
+        }
+        return {-1,-1};
+    }
+};
+
+// TC - O(N)   , SC - O(N) 
