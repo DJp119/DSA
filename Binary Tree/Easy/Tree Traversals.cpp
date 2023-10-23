f69b3e94332d52d485fafd858b56f9b179cf25a7
DJP911 <133709592+DJP911@users.noreply.github.com>
2023-10-23 19:23:41 +0530

    Create Tree Traversals.cpp
    https://www.codingninjas.com/studio/problems/981269?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website

---
+++ Binary Tree/Easy/Tree Traversals.cpp
@@ -0,0 +1,27 @@
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void solve(TreeNode* root,vector<vector<int>> &ans){
+    if(root==NULL) return;
+    ans[1].push_back(root->data);
+    solve(root->left,ans);
+    ans[0].push_back(root->data);
+    solve(root->right,ans);
+    ans[2].push_back(root->data);
+    return;
+}
+vector<vector<int>> getTreeTraversal(TreeNode *root){
+    // Write your code here.
+    vector<vector<int>> ans ={{},{},{}};
+    solve(root,ans);
+    return ans;
+}
