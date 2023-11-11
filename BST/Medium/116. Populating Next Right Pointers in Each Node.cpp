fd2416887ab68881aab71b5c04e14abe7f24af93
DJP911 <133709592+DJP911@users.noreply.github.com>
2023-11-11 12:09:11 +0530

    Create 116. Populating Next Right Pointers in Each Node.cpp
    https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
    
    https://leetcode.com/problems/populating-next-right-pointers-in-each-node/solutions/1653846/c-easy-to-solve-beginner-friendly-with-detailed-explanations-and-dry-run/

---
+++ BST/Medium/116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,39 @@
+// Recursive
+
+class Solution {
+public:
+Node* connect(Node* root) {
+    //base case
+    if(root == NULL) return NULL;
+    //connects the left subtree of same level with right subtree of that same level 
+    if(root->left != NULL) root->left->next = root->right;
+    //connect the rightmost node of a level to the leftmost node of the next level.
+    if(root->right != NULL && root->next != NULL) root->right->next = root->next->left;
+    //recursive calls for left and right subtrees.
+    connect(root->left);
+    connect(root->right);
+    return root;
+   }
+};
+
+// Iterative 
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        //Initialize pointers
+        Node *prev = root, *curr;
+        while (prev) {
+            curr = prev;
+            while (curr && curr->left) { 
+                //connects the left subtree of same level with right subtree of that same level 
+                curr->left->next = curr->right;
+                //connect the rightmost node of a level to the leftmost node of the next level.
+                if (curr -> next) curr->right->next = curr->next->left;
+                curr = curr->next;
+            }
+            prev = prev -> left;
+        }
+        return root;
+    }
+};
