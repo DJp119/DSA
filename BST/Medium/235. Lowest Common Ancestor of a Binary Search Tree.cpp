2cbd1273fdfc0b5374e1d79171e3d4d2e3aaab23
DJP911 <133709592+DJP911@users.noreply.github.com>
2023-11-19 22:45:04 +0530

    Update 235. Lowest Common Ancestor of a Binary Search Tree.cpp

--- BST/Medium/235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ BST/Medium/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -1,12 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 
 class Solution {
 public:
@@ -18,5 +10,23 @@ public:
         }
         return root;
         
+    }
+};
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
+            }
+        }
+        return cur;
     }
 };
