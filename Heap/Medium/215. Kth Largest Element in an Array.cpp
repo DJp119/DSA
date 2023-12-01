3715f8c906294779ce40124cd77a58e52e00aba7
DJP911 <133709592+DJP911@users.noreply.github.com>
2023-12-01 13:44:26 +0530

    Create 215. Kth Largest Element in an Array.cpp
    https://leetcode.com/problems/kth-largest-element-in-an-array/

---
+++ Heap/Medium/215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq(nums.begin(),nums.end());
+        for(int i=0;i<k-1;i++) pq.pop();
+
+        return pq.top();
+
+    }
+};
+class Solution{
+    public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
+        }
+        return pq.top();
+    }
+};
