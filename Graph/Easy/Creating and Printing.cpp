ec8ac1df3faca2d9e639f53e928f9a2c46202f4e
DJP911 <133709592+DJP911@users.noreply.github.com>
2023-12-17 12:10:40 +0530

    Create Creating and Printing.cpp
    https://www.codingninjas.com/studio/problems/create-a-graph-and-print-it_1214551?count=25&page=1&search=&sort_entity=order&sort_order=ASC

---
+++ Graph/Easy/Creating and Printing.cpp
@@ -0,0 +1,20 @@
+vector < vector < int >> printAdjacency(int n, int m, vector < vector < int >> & edges) {
+    vector<int>ans[n];
+    // ans array will store all adjacent nodes corresponding on index
+    for(int i=0;i<m;i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+
+        ans[u].push_back(v);
+        ans[v].push_back(u);
+    }
+    vector<vector<int>>adj(n);
+    for(int i=0;i<n;i++){
+        adj[i].push_back(i);
+        //entering neighbour
+        for(int j=0;j<ans[i].size();j++){
+            adj[i].push_back(ans[i][j]);
+        }
+    }
+    return adj;
+}
