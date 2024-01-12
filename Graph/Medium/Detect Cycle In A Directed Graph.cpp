2cffaf50749913783aa33ca9f6d6661c8bd08498
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-01-12 20:52:01 +0530

    Update Detect Cycle In A Directed Graph.cpp
    https://www.codingninjas.com/studio/problems/detect-cycle-in-a-directed-graph_1062626

--- Graph/Medium/Detect Cycle In A Directed Graph.cpp
+++ Graph/Medium/Detect Cycle In A Directed Graph.cpp
@@ -38,3 +38,43 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
   }
   return false;
 }
+
+
+
+// BFS Method Modified Kahn Algo 
+#include <unordered_map>
+#include <queue>
+#include <list>
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  
+  unordered_map<int,list<int>>adj;
+      
+      for(int i=0;i<edges.size();i++){
+        int u = edges[i].first -1;
+        int v = edges[i].second -1;
+
+        adj[u].push_back(v);
+    }
+  vector<int>indegree(n);
+  for(auto i:adj){
+    for(auto j:i.second) indegree[j]++;
+  }
+
+  queue<int>q;
+  for(int i=0;i<n;i++){
+    if(indegree[i]==0) q.push(i);
+  }
+  int cnt =0;
+  while(!q.empty()){
+    int frontNode = q.front();
+    // inc count 
+    cnt++;
+    q.pop();
+    for(auto neighbour : adj[frontNode]){
+      indegree[neighbour]--;
+      if(indegree[neighbour]==0) q.push(neighbour);
+    }
+  }
+  if(cnt==n) return false;
+  else return true;
+}
