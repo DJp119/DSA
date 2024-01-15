bcf37664358ac7a2d786923e776e5d43bb424c80
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-01-15 21:13:47 +0530

    Create Bridges In A Graph.cpp
    https://www.codingninjas.com/studio/problems/bridges-in-graph_893026

---
+++ Graph/Medium/Bridges In A Graph.cpp
@@ -0,0 +1,63 @@
+#include <unordered_map>
+#include <list>
+
+void dfs(int node, int parent, int &timer, vector<int> &disc, vector<int> &low, vector<vector<int>> &result, unordered_map<int, list<int>> &adj, unordered_map<int, bool> &vis) {
+    vis[node] = true;
+    disc[node] = low[node] = timer++;
+
+    for (auto neighbour : adj[node]) {
+        if (neighbour == parent)
+            continue;
+
+        if (!vis[neighbour]) {
+            dfs(neighbour, node, timer, disc, low, result, adj, vis);
+            low[node] = min(low[node], low[neighbour]);
+
+            // CHECK EDGE IS BRIDGE
+            if (low[neighbour] > disc[node]) {
+                vector<int> ans;
+                ans.push_back(node);
+                ans.push_back(neighbour);
+                result.push_back(ans);
+            }
+        } else {
+            // BACK EDGE
+            low[node] = min(low[node], disc[neighbour]);
+        }
+    }
+}
+
+vector<vector<int>> findBridges(vector<vector<int>> &edges, int v, int e) {
+    // CREATE ADJACENCY LIST
+    unordered_map<int, list<int>> adj;
+
+    for (int i = 0; i < edges.size(); i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int timer = 0;
+    vector<int> disc(v);
+    vector<int> low(v);
+    int parent = -1;
+    unordered_map<int, bool> vis;
+
+    for (int i = 0; i < v; i++) {
+        disc[i] = -1;
+        low[i] = -1;
+    }
+
+    vector<vector<int>> result;
+
+    // DFS
+    for (int i = 0; i < v; i++) {
+        if (!vis[i]) {
+            dfs(i, parent, timer, disc, low, result, adj, vis);
+        }
+    }
+
+    return result;
+}
