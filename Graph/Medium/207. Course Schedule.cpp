72364bd074e706af01e68cafa189e0d069594cc0
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-01-21 17:24:00 +0530

    Update 207. Course Schedule.cpp
    https://leetcode.com/problems/course-schedule/

--- Graph/Medium/207. Course Schedule.cpp
+++ Graph/Medium/207. Course Schedule.cpp
@@ -32,3 +32,48 @@ public:
         return count == numCourses;
     }
 };
+
+
+// DFS 
+class Solution {
+    bool checkDFS(int node, unordered_map<int, bool>& visited, unordered_map<int, bool>& dfsvisited, unordered_map<int, list<int>>& adj) {
+        visited[node] = true;
+        dfsvisited[node] = true;
+        
+        for (auto neighbour : adj[node]) {
+            if (!visited[neighbour]) {
+                bool cycleDet = checkDFS(neighbour, visited, dfsvisited, adj);
+                if (cycleDet) return true;
+            } else if (dfsvisited[neighbour]) {
+                return true;  // If the neighbour is visited in the current DFS traversal, then it's a back edge, indicating a cycle
+            }
+        }
+        
+        dfsvisited[node] = false;  // Move this line inside the loop
+        return false;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, list<int>> adj;
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[u].push_back(v);
+        }
+
+        unordered_map<int, bool> visited;
+        unordered_map<int, bool> dfsvisited;
+        int size = numCourses;
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (!visited[i]) {
+                bool cycleDet = checkDFS(i, visited, dfsvisited, adj);
+                if (cycleDet) return false;  // If there is a cycle, it's impossible to finish all courses
+            }
+        }
+
+        return true;  // If no cycle is detected, it's possible to finish all courses
+    }
+};
