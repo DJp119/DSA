847dd1a685b239846efb856ab738232d4bfa0a4f
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-02-22 20:10:41 +0530

    Create if-else (Decision Making).cpp
    https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM

---
+++ Basics/Easy/if-else (Decision Making).cpp
@@ -0,0 +1,17 @@
+/*
+    Time complexity: O(1)
+    Space complexity: O(1)
+*/
+
+string compareIfElse(int a, int b) {
+
+    if (a > b)
+        return "greater";
+    else if (a < b)
+        return "smaller";
+    else
+        return "equal";
+
+}
+
+https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
