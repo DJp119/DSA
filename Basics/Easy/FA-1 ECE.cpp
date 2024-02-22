fe84fb425b2b6f21ae1a77f53b359951aa770455
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-02-22 20:56:39 +0530

    Create FA-1 ECE.cpp
    https://www.codingninjas.com/studio/problems/fa-1-ece_4606261?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM

---
+++ Basics/Easy/FA-1 ECE.cpp
@@ -0,0 +1,16 @@
+int Maximum(int x, int y)
+{
+	return max(x,y);
+	// if(x>y) return x;
+	// else y;
+}
+void Swap(int &x, int &y)
+{
+	swap(x,y);
+
+	// int k = x;
+	// x=y;
+	// y=k;
+}
+
+https://www.codingninjas.com/studio/problems/fa-1-ece_4606261?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
