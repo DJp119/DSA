184a191b4a67ae9d8373c448f634cbc72bda57e7
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-02-22 20:22:42 +0530

    Create Switch Case statement.cpp
    https://www.codingninjas.com/studio/problems/switch-case-statement_8357244?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM

---
+++ Basics/Easy/Switch Case statement.cpp
@@ -0,0 +1,15 @@
+double areaSwitchCase(int ch, vector<double> a) {
+	// double area = 0.0;
+
+	switch(ch){
+		case 1 : 
+		return  M_PI * a[0]*a[0];
+		break;
+
+		case 2 :
+		return  a[0]*a[1];
+	}
+	// return area;
+}
+
+https://www.codingninjas.com/studio/problems/switch-case-statement_8357244?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
