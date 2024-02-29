339ff33d8fca8ac38ec6f3d0c3977d7239ebd803
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-03-01 00:59:11 +0530

    Update 53. Maximum Subarray.cpp
    https://leetcode.com/problems/maximum-subarray/solutions/20360/c-an-clear-o-n-divide-and-conquer-solution-with-comments/

--- Array/Medium/53. Maximum Subarray.cpp
+++ Array/Medium/53. Maximum Subarray.cpp
@@ -9,3 +9,32 @@ public:
         return globalMaxSum;
     }
 }; 
+
+
+class Solution {
+public:
+    void maxSubArray(vector<int>& nums, int l, int r, int& mx, int& lmx, int& rmx, int& sum) {
+        if (l == r) {
+            mx = lmx = rmx = sum = nums[l];
+        }
+        else {
+            int m = (l + r) / 2;
+            int mx1, lmx1, rmx1, sum1;
+            int mx2, lmx2, rmx2, sum2;
+            maxSubArray(nums, l, m, mx1, lmx1, rmx1, sum1);
+            maxSubArray(nums, m + 1, r, mx2, lmx2, rmx2, sum2);
+            mx = max(max(mx1, mx2), rmx1 + lmx2);
+            lmx = max(lmx1, sum1 + lmx2);
+            rmx = max(rmx2, sum2 + rmx1);
+            sum = sum1 + sum2;
+        }
+    }
+    int maxSubArray(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        int mx, lmx, rmx, sum;
+        maxSubArray(nums, 0, nums.size() - 1, mx, lmx, rmx, sum);
+        return mx;
+    }
+};
