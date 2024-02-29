af229582fa5c70bd35af50e20c57bbac9b17c056
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-03-01 01:19:57 +0530

    Update 121.Stock Buy and Sell.cpp
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
    
    Create a variable maxPro and store 0 initially.
    Create a variable minPrice and store some larger value(ex: MAX_VALUE) value initially.
    Run a for loop from 0 to n.
    Update the minPrice if it is greater than the current element of the array
    Take the difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
    Return the maxPro.

--- Array/Medium/121.Stock Buy and Sell.cpp
+++ Array/Medium/121.Stock Buy and Sell.cpp
@@ -1,3 +1,17 @@
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+            maxPro = max(arr[j] - arr[i], maxPro);
+            }
+        }
+        }
+
+    return maxPro;
+}
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -20,3 +34,14 @@ int main() {
     int maxPro = maxProfit(arr);
     cout << "Max profit is: " << maxPro << endl;
 }
+
+
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Create a variable maxPro and store 0 initially.
+Create a variable minPrice and store some larger value(ex: MAX_VALUE) value initially.
+Run a for loop from 0 to n.
+Update the minPrice if it is greater than the current element of the array
+Take the difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
+Return the maxPro.
