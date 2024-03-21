009496b5c40e73c919f7ac0ab834bfabbda6c710
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-03-21 22:12:01 +0530

    Update 14. Longest Common Prefix.cpp
    https://leetcode.com/problems/longest-common-prefix/

--- String/Easy/14. Longest Common Prefix.cpp
+++ String/Easy/14. Longest Common Prefix.cpp
@@ -13,3 +13,39 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans ;
+        sort(strs.begin(),strs.end());
+        for(int i = 0 ; i<strs.size();i++){
+            cout<<strs[i];
+        }
+        int start = 0;
+        int end = strs.size()-1;
+
+        string start_string = strs[start];
+        string end_string = strs[end];
+        for(int i = 0 ; i<end_string.size();i++){
+            if(start_string[i]==end_string[i]) ans+=start_string[i];
+            else break;
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = strs[0];
+        for (string s : strs)
+            while (s.find(prefix) != 0)
+                prefix = prefix.substr(0, prefix.length() - 1);
+        return prefix;
+
+    }
+};
+
+https://leetcode.com/problems/longest-common-prefix/
