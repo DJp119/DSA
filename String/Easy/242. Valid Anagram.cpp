8a18a57e3de77e7e6bba7393d2aff3e88c9029db
DJP911 <133709592+DJP911@users.noreply.github.com>
2024-03-21 23:09:57 +0530

    Update 242. Valid Anagram.cpp
    https://leetcode.com/problems/valid-anagram/description/

--- String/Easy/242. Valid Anagram.cpp
+++ String/Easy/242. Valid Anagram.cpp
@@ -22,3 +22,95 @@ class Solution{
         return true;
     }
 };
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        map<char,int>mymap1;
+        map<char,int>mymap2;
+        for(auto it:s) {
+            mymap1[it]++;
+        }
+        for(auto it:t){
+            mymap2[it]++;
+        }
+        return mymap1 == mymap2;
+        // Wrong logic as it was , i am finding the only the occurence the frequency sholud aslo be checked 
+        // bool ans =  true;
+        // for(auto it:mymap1){
+        //     if(mymap2.find(it.first)!=mymap2.end()) {
+        //         ans = true; 
+        //     }
+        //     else {
+        //         ans = false; break;
+        //     }
+        // }
+
+        // return ans;
+    }
+};
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> count;
+        
+        // Count the frequency of characters in string s
+        for (auto x : s) {
+            count[x]++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (auto x : t) {
+            count[x]--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (auto x : count) {
+            if (x.second != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        
+        // Count the frequency of characters in string s
+        for (char x : s) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+
+https://leetcode.com/problems/valid-anagram/description/
