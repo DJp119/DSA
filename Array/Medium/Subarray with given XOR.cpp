e14be054e38e9bf868063f8fccf841f962c5040c
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2024-06-02 21:02:56 +0530

    Create Subarray with given XOR.cpp
    https://www.interviewbit.com/problems/subarray-with-given-xor/

---
+++ Array/Medium/Subarray with given XOR.cpp
@@ -0,0 +1,26 @@
+https://www.interviewbit.com/problems/subarray-with-given-xor/
+
+int Solution::solve(vector<int> &A, int B) {
+    int n = A.size(); //size of the given array.
+    int xr = 0;
+    map<int, int> mpp; //declaring the map.
+    mpp[xr]++; //setting the value of 0.
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        // prefix XOR till index i:
+        xr = xr ^ A[i];
+
+        //By formula: x = xr^k:
+        int x = xr ^ B;
+
+        // add the occurrence of xr^k
+        // to the count:
+        cnt += mpp[x];
+
+        // Insert the prefix xor till index i
+        // into the map:
+        mpp[xr]++;
+    }
+    return cnt;
+}
