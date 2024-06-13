cce83c1f13bbc82128a939e061ef0bff8b1cc67d
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2024-06-14 00:52:45 +0530

    Create Minimum Platforms.cpp
    https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1

---
+++ Greedy/Medium/Minimum Platforms.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	sort(arr,arr+n);
+    	sort(dep,dep+n);
+    	int cnt = 0 , maxcount = 0 ;
+    	int i = 0 , j = 0 ;
+    	while(i<n){
+    	    if(arr[i]<=dep[j]) cnt++,i++;
+    	    else cnt--,j++;
+    	    maxcount = max(cnt,maxcount);
+    	}
+    	return maxcount;
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
+
+
+https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1
