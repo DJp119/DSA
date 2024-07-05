a21de4487c20ac00e14c2ce1f686bce5ae3eccb3
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2024-07-05 23:18:02 +0530

    Create 459. Repeated Substring Pattern.cpp
    https://leetcode.com/problems/repeated-substring-pattern/

---
+++ String/Easy/459. Repeated Substring Pattern.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string doubled = s + s;
+        string sub = doubled.substr(1, doubled.size() - 2);
+        return sub.find(s) != string::npos;
+    }
+};
+
+https://leetcode.com/problems/repeated-substring-pattern/
