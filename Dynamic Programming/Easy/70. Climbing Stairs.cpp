7ac1e57f2ff02fcd082b18cdfecda4cc840b3c87
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2024-08-12 19:27:45 +0530

    Create 70. Climbing Stairs.cpp
    https://leetcode.com/problems/climbing-stairs/description/

---
+++ Dynamic Programming/Easy/70. Climbing Stairs.cpp
@@ -0,0 +1,67 @@
+Approach 1: Recursion 
+  class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+};
+
+Approach 2: Memoization
+
+class Solution {
+public:
+    int climbStairs(int n, unordered_map<int, int>& memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (memo.find(n) == memo.end()) {
+            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        }
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairs(n, memo);
+    }
+};
+
+Approach 3: Tabulation
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+Approach 4: Space Optimization
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+};
+
+https://leetcode.com/problems/climbing-stairs/description/
+https://leetcode.com/problems/climbing-stairs/solutions/3708750/4-method-s-beat-s-100-c-java-python-beginner-friendly/
